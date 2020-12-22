b3ce0ef9b966d2ba556d049c05b70163b5a74476
jmelrose8094 <melrosj@sunypoly.edu>
2020-12-21 20:52:45 -0500

    Fixed Camera angle and Movement

--- ForestDungeonGame/Assets/Player/PlayerController.cs
+++ ForestDungeonGame/Assets/Player/PlayerController.cs
@@ -8,10 +8,10 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate()
     {
         Vector3 force = new Vector3();
-        if (Input.GetKey(KeyCode.A))       force.x = -1000;
-        else if (Input.GetKey(KeyCode.D))  force.x = 1000;
-        if (Input.GetKey(KeyCode.S))       force.z = -1000;
-        else if (Input.GetKey(KeyCode.W))  force.z = 1000;
+        if (Input.GetKey(KeyCode.A))       force.x = 1000;
+        else if (Input.GetKey(KeyCode.D))  force.x = -1000;
+        if (Input.GetKey(KeyCode.S))       force.y = -1000;
+        else if (Input.GetKey(KeyCode.W))  force.y = 1000;
         rb.AddForce(force * Time.deltaTime);
     }
 }
